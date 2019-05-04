13045e80819b78f100d5479a65f8040727033d51
sanjithpk <sanjithpk06@gmail.com>
2019-05-04 16:43:12 +0530

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() {
+  runApp(MaterialApp(
+    title: "My New App",
+    theme: ThemeData(primarySwatch: Colors.red, brightness: Brightness.light),
+    home: HomeWidget()
+  ));
+
+}
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+  
+}
+
+class _HomeWidgetState extends State<HomeWidget>{
+  bool color = true;
+  @override
+  void initState(){
+    super.initState();
+    print("Working");
+    fetchData();
+  }
+
+  fetchData()async{
+    var url = "http://dummy.restapiexample.com/api/v1/employees";
+    print("before");
+    var response = await http.get(url);
+    print("response $response");
+    print("after");
+
+    if(response.statusCode == 200){
+      var jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+    } else {
+      print("Request failed with status : ${response.statusCode}.");
+    }
+
+  }
+
+
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: color ? Colors.blue : Colors.black,
+        child: Text(color.toString(), style : TextStyle(fontSize: 40,color: Colors.white)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.cyan,
+        onPressed: (){
+          setState(() {
+           color = !color; 
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+// class HomeWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       drawer: Drawer(),
+//       //backgroundColor: Colors.cyan,
+//       appBar: AppBar(
+//         title: Text("My Header"),
+//       ),
+//         body: ListView.builder(
+//           itemCount: 50,
+//           itemBuilder: (context,index){
+//             return Card(
+//               child: ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundImage: // Container(decoration: BoxDecoration(image: DecorationImage(image:)),)// Image.asset("assets.pic1.jpg",),
+//                 ),
+//                 trailing: Text("2:10"),
+//                 title: Text("User Name"),
+//                 subtitle: Text("Subtitle"),
+//               ),);
+            
+//             // return Row(
+//             //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             //   children: [
+//             //     Row(
+//             //       children:     <Widget>[                
+//             //         CircleAvatar(    
+//             //           backgroundColor: Colors.blue,    
+//             //           child: Text("$index"),    
+//             //         ),
+//             //         Column(
+//             //           children: <Widget>[
+//             //             Padding(
+//             //               padding: const EdgeInsets.all(top:8.0,left:
+//             //                 Text("User Name"),
+//             //               )
+                           
+
+//             //             ),
+//             //             Text("")
+//             //           ],
+//             //         ),  
+//             //       ],
+//             //      ),
+//             //      Padding(
+//             //        padding: const EdgeInsets.all(18.0),
+//             //        child: Text("2:10"),
+//             //      )
+//             //     ],
+//             //   );
+//             },
+//         ),
+        
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: (){},
+//         child: Icon(Icons.add),),
+//     );
+//   }
+// }
